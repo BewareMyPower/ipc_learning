04ddd74ed9482ba00bfd88789da582896b3fc94f
BewareMyPower <xyzinfernity@163.com>
2018-09-25 10:57:47 +0800

    用Poller类简化消息队列的IO多路复用，支持监听多个消息队列

--- msgqueue/epoll_consumer.cc
+++ msgqueue/epoll_consumer.cc
@@ -4,76 +4,69 @@
 #include <sys/epoll.h>
 #include <unistd.h>
 #include <string>
+#include <unordered_map>
 #include "error_handler.hpp"
-
-class MyData {
-public:
-    MyData(int fd, const std::string& name) : fd_(fd), name_(name) {}
-
-    int getFd() const { return fd_; }
-    std::string getName() const { return name_ + "[" + std::to_string(fd_) + "]"; }
-
-private:
-    int fd_;
-    std::string name_;
-};
-
-static volatile bool do_receive = true;
+#include "poller.h"
 
 int main(int argc, char* argv[]) {
-    if (argc != 2 || strcmp(argv[1], "--help") == 0)
-        errorExit("Usage: %s mq-name", argv[0]);
+    if (argc < 2 || strcmp(argv[1], "--help") == 0)
+        errorExit("Usage: %s mq-name...", argv[0]);
 
-    // 创建消息队列
-    auto mqd = mq_open(argv[1], O_RDONLY | O_NONBLOCK);
-    if (mqd == -1) errorExit(errno, "mq_open \"%s\"", argv[0]);
+    std::vector<int> mqds;  // 消息队列描述符列表
+    // 描述符到消息队列名称的映射
+    std::unordered_map<int, std::string> mqd_to_name;
+    // 描述符到消息大小的映射
+    std::unordered_map<int, size_t> mqd_to_bufsize;
 
-    // 创建合适的缓冲区
+    std::vector<char> buffer(1024);  // 消息缓冲区
+
     struct mq_attr attr;
-    if (mq_getattr(mqd, &attr) == -1)
-        errorExit(errno, "mq_getattr");
-    char* buffer = new char[attr.mq_msgsize + 1];
+    for (int i = 1; i < argc; i++) {
+        // 创建消息队列
+        const char* mq_name = argv[i];
+        int mqd = mq_open(mq_name, O_RDONLY | O_NONBLOCK);
+        if (mqd == -1)
+            errorExit(errno, "mq_open \"%s\"", mq_name);
 
-    // 消息队列关联数据
-    MyData mqdData{mqd, "Message Queue " + std::to_string(mqd)};
+        // 取得消息最大大小
+        if (mq_getattr(mqd, &attr) == -1)
+            errorExit(errno, "mq_getattr %d %s", mqd, mq_name);
 
-    // 创建epoll对象
-    auto epfd = epoll_create1(0);
+        // 更新消息队列相应结构
+        mqd_to_name.emplace(mqd, mq_name);
+        mqd_to_bufsize.emplace(mqd, attr.mq_msgsize);
+        mqds.emplace_back(mqd);
 
-    // 监听消息队列描述符和关联数据结构
-    struct epoll_event ev;
-    ev.events = EPOLLIN;
-    ev.data.ptr = &mqdData;
-    if (epoll_ctl(epfd, EPOLL_CTL_ADD, mqd, &ev) == -1)
-        errorExit(errno, "epoll_ctl");
+        // 确立合适的缓冲区大小
+        if (attr.mq_msgsize >= static_cast<long>(buffer.size()))
+            buffer.resize(attr.mq_msgsize + 1);
+    }
 
-    // 用信号Ctrl+C来安全终止循环
-    signal(SIGINT, [](int) { do_receive = false; });
+    // 创建epoll对象并监听消息队列
+    Poller poller;
+    for (int mqd : mqds)
+        poller.add(mqd, EPOLLIN);
+    poller.registerInterruptSignal(SIGINT);
 
     // 监听读取事件
     ssize_t num_read;
-    while (do_receive) {
-epoll_wait_again:
-        if (epoll_wait(epfd, &ev, 1, -1) == -1) {
-            if (errno == EINTR) {
-                if (do_receive)
-                    goto epoll_wait_again;
-                else  // SIGINT信号
-                    break;
-            }
-            else
-                errorExit(errno, "epoll_wait");
-        }
+    while (true) {
+        auto events = poller.poll(-1);
+        if (events.size() == 0)
+            break;
 
         // 消费消息队列
-        while ((num_read = mq_receive(mqd, buffer, attr.mq_msgsize, NULL)) >= 0) {
-            buffer[num_read] = '\0';
-            printf("%s receives: %s\n", mqdData.getName().c_str(), buffer);
+        for (auto& ev : events) {
+            int mqd = ev.data.fd;
+            while ((num_read = mq_receive(mqd, &buffer[0], mqd_to_bufsize.at(mqd), NULL)) >= 0) {
+                buffer[num_read] = '\0';
+                printf("%s receives: %s\n", mqd_to_name[mqd].c_str(), buffer.data());
+            }
+            if (errno != EAGAIN)
+                errorExit(errno, "mq_receive");
         }
-
-        if (errno != EAGAIN) errorExit(errno, "mq_receive");
     }
 
-    printf("%s stop receiving\n", mqdData.getName().c_str());
+    printf("Stop receiving\n");
     return 0;
 }
